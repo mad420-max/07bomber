c2e5a08cbd93ae048d4d9effd31db891851d34fc
mad420-max <78628376+mad420-max@users.noreply.github.com>
2022-01-20 20:37:38 +0600

    Update 007bomber.py

--- 007bomber.py
+++ 007bomber.py
@@ -25,12 +25,4 @@ print (cyan+"""
 ------------------------------------------------
 """)
 
-phone  = input("phone number: ")
-amount = int(input("amount: "))
-
-url ="https://www.bioscopelive.com/bn/login/send-otp?phone=88"+phone+"&operator=bd-otp"
-
-for a in range(sms):
-	request.urlopen(url)
-	print(str(a+1)+ "sms send ")
-	time.sleep(30)
+print " removed😁"
